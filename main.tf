71b511a69ba767a4d809486c0887288fb86a6fd3
Tomas Miklusicak <tomas.miklusicak@concentric.ai>
2025-12-10 15:43:47 +0100

    Add readme

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
 module "asg" {
-  source  = "git::https://github.com/tmiklu/terraform-modules.git//terraform-aws-autoscaling?ref=main"
+  source  = "git::https://github.com/tmiklu/terraform-modules.git//terraform-aws-autoscaling-9.0.2?ref=main"
+}
+
+module "kms" {
+  source  = "git::https://github.com/tmiklu/terraform-modules.git//terraform-aws-kms-4.1.1?ref=main"
 }
